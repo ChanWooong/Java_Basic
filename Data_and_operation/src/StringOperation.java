3bcbb3afe31d86b3549eea5026bc9efffc3f73a4
ChanWooong <oco466kr@knu.ac.kr>
2022-06-20 19:13:44 +0900

    used length, replace in string
    when we used the string type,
    replace() is a function of switch value and variable.
    length() is a function of calculate string length.

---
+++ Data_and_operation/src/StringOperation.java
@@ -0,0 +1,11 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  	  System.out.println("Hello World".length()); // 11    System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "duru"));
+      System.out.println("Hello, [[[name]]] ... bye. ".replace("[[[name]]]", "duru"));
+	}
+	// .length()를 이용하면 문자열의 길이를 구할 수 있다.
+	// .replace("[[variable]]", "value") 이렇게 하면 변수가 지정된 곳에 특정 값을 넣을 수 있다.
+}
