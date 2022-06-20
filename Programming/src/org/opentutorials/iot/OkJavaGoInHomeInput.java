75dabd98a912dc406fdb687578ac8d3c3b327358
ChanWooong <oco466kr@knu.ac.kr>
2022-06-20 22:32:30 +0900

    programming part / learn about input

---
+++ Programming/src/org/opentutorials/iot/OkJavaGoInHomeInput.java
@@ -0,0 +1,36 @@
+import javax.swing.JOptionPane;	//JOptionPan 객체를 사용하기 위해서 import하는 것.
+ 
+import org.opentutorials.iot.DimmingLights;
+import org.opentutorials.iot.Elevator;
+import org.opentutorials.iot.Lighting;
+import org.opentutorials.iot.Security;
+ 
+public class OkJavaGoInHomeInput {
+ 
+    public static void main(String[] args) {
+         
+        String id = JOptionPane.showInputDialog("Enter a ID");	//showInputDialog를 이용하면 string 정보를 얻을 수 있다.
+        String bright = JOptionPane.showInputDialog("Enter a Bright level");
+         
+        // Elevator call 
+        Elevator myElevator = new Elevator(id);
+        myElevator.callForUp(1);
+         
+        // Security off 
+        Security mySecurity = new Security(id);
+        mySecurity.off();
+         
+        // Light on
+        Lighting hallLamp = new Lighting(id+" / Hall Lamp");
+        hallLamp.on();
+         
+        Lighting floorLamp = new Lighting(id+" / floorLamp");
+        floorLamp.on();
+         
+        DimmingLights moodLamp = new DimmingLights(id+" moodLamp");
+        moodLamp.setBright(Double.parseDouble(bright));
+        moodLamp.on();
+ 
+    }
+ 
+}
