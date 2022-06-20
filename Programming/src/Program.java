2676ab42107b89ab8c67c55beb61de846496a5f2
ChanWooong <oco466kr@knu.ac.kr>
2022-06-20 20:36:12 +0900

    add egoing's file, and create IoT module
    practice about Java programming(automation)

---
+++ Programming/src/Program.java
@@ -0,0 +1,12 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+	}
+
+}
+//자동화가 가능할 수 있도록 해주는 것이 java.
