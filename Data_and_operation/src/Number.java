b19cf79ea04bbc0ecaa8755907ddb9ee0f7eca29
ChanWooong <oco466kr@knu.ac.kr>
2022-06-20 18:14:03 +0900

    create Number class, and use Math library

---
+++ Data_and_operation/src/Number.java
@@ -0,0 +1,28 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println(6 + 2); // 8
+	     System.out.println(6 - 2); // 4
+	     System.out.println(6 * 2); // 12
+	     System.out.println(6 / 2); // 3
+	 
+	     System.out.println(Math.PI); // 3.141592653589793
+	     System.out.println(Math.floor(Math.PI));       System.out.println(Math.ceil(Math.PI));
+
+	}
+}
+
+// Math.abs() : 인수의 절댓값 반환
+// Math.pow() : 거듭제곱
+// Math.max(), Math.min() : 최댓값, 최솟값 반환
+//Math.sin() : ()안의 값의 sin값을 나타낸다(단, ()안의 값은 radian 값이다.)
+//Math.cos() : ()안의 값의 cos값을 나타낸다(단, ()안의 값은 radian 값이다.)
+//Math.tan() : ()안의 값의 tan값을 나타낸다(단, ()안의 값은 radian 값이다.)
+//Math.sqrt() : ()안의 루트값.    sqrt: square root
+//Math.cbrt() : ()안의 세제곱근값.   cbrt: cubic root
+//Math. exp() : e^()
+//Math. copysign(x , y) : copysign, 즉 "부호를 카피하다"라는 뜻으로 x의 부호를 y의 부호와 같게 출력한다.
+
+
