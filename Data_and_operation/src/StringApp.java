0d6a6b50d257823411bb22a1e158a0580f3db358
ChanWooong <oco466kr@knu.ac.kr>
2022-06-20 18:30:18 +0900

    create String class and testing

---
+++ Data_and_operation/src/StringApp.java
@@ -0,0 +1,20 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World"); // String
+        System.out.println('H'); // Character
+        System.out.println("H"); 
+     
+        System.out.println("Hello " + "World");
+         
+        // new line
+        System.out.println("Hello \nWorld");
+         
+        // escape
+        System.out.println("Hello \"World\"");// Hello "World"
+    }
+}
+// 전체적으로 \"나 \n과 같은 개행 문자는 c언어와 유사함.
+
