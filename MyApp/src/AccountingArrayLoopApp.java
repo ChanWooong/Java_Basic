3e6fa1b72db2f47370e7faed9953f7296a8a4c7e
ChanWooong <oco466kr@knu.ac.kr>
2022-06-23 02:34:40 +0900

    App making 6 - use loop

--- MyApp/src/AccountingArrayLoopApp.java
+++ MyApp/src/AccountingArrayLoopApp.java
@@ -12,24 +12,31 @@ public class AccountingArrayLoopApp {
 		double total = valueOfSupply + vat;
 		double expense = valueOfSupply * expenseRate;
 		double income = valueOfSupply - expense;
-//		double rate1 = 0.5;
-//		double rate2 = 0.3;
-//		double rate3 = 0.2;
-		
-		double[] dividendRates = new double[3];
-		
-		double dividend1 = income * dividendRates[0];
-		double dividend2 = income * dividendRates[1];
-		double dividend3 = income * dividendRates[2];
+
 
 		System.out.println("Value of supply : "+valueOfSupply);
 		System.out.println("VAT : "+vat);
 		System.out.println("Total : "+total);
 		System.out.println("Expense : "+expense);
 		System.out.println("Income : "+income);
-		System.out.println("Dividend 1 : "+dividend1);
-		System.out.println("Dividend 2 : "+dividend2);
-		System.out.println("Dividend 3 : "+dividend3);
+		
+//		double rate1 = 0.5;
+//		double rate2 = 0.3;
+//		double rate3 = 0.2;
+		
+		double[] dividendRates = new double[3];
+		dividendRates[0] = 0.5;
+		dividendRates[0] = 0.3;
+		dividendRates[0] = 0.2;
+		double dividend1 = income * dividendRates[0];
+		double dividend2 = income * dividendRates[1];
+		double dividend3 = income * dividendRates[2];
+		
+		int i = 0;
+		while(i < dividendRates.length) {
+			System.out.println("Dividend : "+ (income * dividendRates[i]));
+			i += 1;
+		}
 
 	}
 
