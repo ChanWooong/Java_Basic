6e7a91c03efd1f8a002c5ca5a815c3151623eb45
ChanWooong <oco466kr@knu.ac.kr>
2022-06-23 02:17:50 +0900

    App making 4 - use if

--- MyApp/src/AccountingIfApp.java
+++ MyApp/src/AccountingIfApp.java
@@ -1,5 +1,5 @@
 
-public class AccountingApp {
+public class AccountingIfApp {
 // 부가가치세를 책정하는 class.
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,10 +12,19 @@ public class AccountingApp {
 		double total = valueOfSupply + vat;
 		double expense = valueOfSupply * expenseRate;
 		double income = valueOfSupply - expense;
-		double dividend1 = income * 0.5;
-		double dividend2 = income * 0.3;
-		double dividend3 = income * 0.2;
-
+		
+		double dividend1;
+		double dividend2;
+		double dividend3;
+		if(income > 10000.0) {
+			dividend1 = income * 0.5;
+			dividend2 = income * 0.3;
+			dividend3 = income * 0.2;
+		} else {
+			dividend1 = income * 1.0;
+			dividend2 = income * 0;
+			dividend3 = income * 0;
+		}
 		System.out.println("Value of supply : "+valueOfSupply);
 		System.out.println("VAT : "+vat);
 		System.out.println("Total : "+total);
