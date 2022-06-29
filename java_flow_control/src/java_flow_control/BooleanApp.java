707d90e31021aeb9202e589e0f8ecd2a27a374ab
ChanWooong <oco466kr@knu.ac.kr>
2022-06-29 12:04:56 +0900

    JAVA2 - flow control

---
+++ java_flow_control/src/java_flow_control/BooleanApp.java
@@ -0,0 +1,20 @@
+package java_flow_control;
+
+public class BooleanApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("One");
+		System.out.println(1);
+		
+		System.out.println(true);
+		System.out.println(false);
+		
+		String foo = "Hello World";
+		// String true = "Hello World"; reserved word
+		System.out.println(foo.contains("World"));
+		System.out.println(foo.contains("egoing"));
+		//contains methods : 문자열에 사용하며 결과값이 boolean이 도출됨.
+	}
+
+}
